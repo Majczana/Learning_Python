a751f41a24709fd6964e39a7cce8a5a009a80602
Filip Majewski <majewskifilip777@gmail.com>
2024-12-30 23:49:49 +0100

    Segregacja

---
+++ PROJECTS/Guessing Game.py
@@ -0,0 +1,14 @@
+import random
+
+food = ["Pizza","Pasta","Hamburger","Hot Dog","Frytki","Kebab","Tacos","Sushi","Ramen","Sałatka Cezar","Kurczak Tandoori","Lasagna","Risotto","Chili con Carne","Zupa pomidorowa","Żurek","Placki ziemniaczane","Gołąbki","Kaszotto","Bigos","Pierogi","Naleśniki","Stek","Ryba z frytkami","Krewetki","Falafel","Omlet","Bagietka","Croissant","Czekoladowe Brownie"]
+word = food[random.randint(0, 30)]
+
+guesses = ""
+attempts = 0
+
+#user_name = input("Podaj swoje imię: ").capitalize
+#print(f"Powodzenia {user_name}!")
+
+
+
+
