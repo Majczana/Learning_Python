60823eccc4d70340d08d4f2d56655b53c80843d9
Filip Majewski <majewskifilip777@gmail.com>
2024-12-30 21:45:24 +0100

    Zadanie_2 end

--- Learning function/Zadanie_2.py
+++ Learning function/Zadanie_2.py
@@ -1,3 +1,30 @@
+def temp_change(temp, unit):
+    if unit == "C":
+        return round(temp * (9/5) + 32, 1)
+    elif unit == "F":
+        return round((temp - 32) * (5/9), 1)
 
-name = "siema"
-print(name)
+while True:
+
+    unit = input("Podaj jednostkę (C/F):").upper()
+    if unit not in ["C","F"]:
+        print(f"{unit} to zła jednostka.")
+        continue
+
+    temp = input(f"Podaj temperature w {unit}: ")
+
+    try:
+        temp = float(temp)
+    except ValueError:
+        print(f"{temp} nie jest liczbą. Spróbuj ponownie.")
+        continue
+
+    print(temp_change(temp, unit))
+
+    print("Enter to continue")
+    print("Exit to leave")
+
+    decyzja = input("").lower()
+    if decyzja == "exit":
+        print("Dzięki za przetestowanie")
+        break
