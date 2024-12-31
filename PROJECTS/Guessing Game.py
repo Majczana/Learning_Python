f2c775fa473fea007a80d5123b0d3d22cfaa2158
Filip Majewski <majewskifilip777@gmail.com>
2024-12-31 02:05:12 +0100

    stupid shit

--- PROJECTS/Guessing Game.py
+++ PROJECTS/Guessing Game.py
@@ -1,14 +1,31 @@
 import random
 
-food = ["Pizza","Pasta","Hamburger","Hot Dog","Frytki","Kebab","Tacos","Sushi","Ramen","Sałatka Cezar","Kurczak Tandoori","Lasagna","Risotto","Chili con Carne","Zupa pomidorowa","Żurek","Placki ziemniaczane","Gołąbki","Kaszotto","Bigos","Pierogi","Naleśniki","Stek","Ryba z frytkami","Krewetki","Falafel","Omlet","Bagietka","Croissant","Czekoladowe Brownie"]
-word = food[random.randint(0, 30)]
-
-guesses = ""
+food = ["pizza","pasta","hamburger"]
+word = food[random.randint(0, 2)] # Od 0 - 2 bo lista zaczyna się od 0
 attempts = 0
+guesses = []
+word_encript = word
 
 #user_name = input("Podaj swoje imię: ").capitalize
 #print(f"Powodzenia {user_name}!")
 
 
+for i in range (len(word_encript)):
+    word_encript = word_encript[0:i] + "_" + word_encript[i:1]
+print(word_encript)
 
 
+while "_" in word_encript:
+    user_input = input("podaj litere")
+
+    if user_input in guesses:
+        (print("juz podales ta litere"))
+        continue
+    
+    guesses.append(user_input)
+
+    if user_input in word:
+        for index, char in enumerate(word):
+            if char == user_input:
+                guesses[index] = user_input
+        print("".join(hidden_word))
